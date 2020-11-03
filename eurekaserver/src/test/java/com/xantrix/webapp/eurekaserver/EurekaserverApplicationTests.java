5e36117984872438dc83ed68c9d70687e678cf81
UOrnaghiProj <umberto.ornaghi@gmail.com>
2020-11-03 19:03:47 +0100

    Inizializzazione del Eureka server con le configurazioni di base

---
+++ eurekaserver/src/test/java/com/xantrix/webapp/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xantrix.webapp.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
